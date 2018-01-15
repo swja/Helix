7a6682bf03a5bcc421ec88e0fc3fe73c477d9564
Santiago Jara <swja1991@hotmail.com>
2018-01-15 09:26:21 -0500

    Pruebas para branch test

--- Helix/Login.aspx.cs
+++ Helix/Login.aspx.cs
@@ -8,7 +8,7 @@ public partial class Login : System.Web.UI.Page
 
     }
 
-    protected void button_Click(object sender, EventArgs e)
+    protected void Button_Click(object sender, EventArgs e)
     {
         ScriptManager.RegisterStartupScript(this, this.GetType(), "alertIns", "alert('El registro se guardó correctamente');", true);
         //mesage para probar git
